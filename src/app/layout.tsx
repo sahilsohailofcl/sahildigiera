035809e9f2f93ff56aedbeadf6aa3e45f28b77f3
sahilsohailofcl <sahilsohailofcl@gmail.com>
2025-03-15 14:53:46 +0000

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import Script from "next/script"; // Import the Script component
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
@@ -17,6 +18,22 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative" style={{ scrollBehavior: "smooth" }}>
+      <head>
+        {/* Google Analytics Script */}
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=G-D17PMQF9Y4`}
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-D17PMQF9Y4');
+          `}
+        </Script>
+      </head>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
       </body>
