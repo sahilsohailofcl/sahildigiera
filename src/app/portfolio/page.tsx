96864e0780ddf6e8506c47e2f44fafa5cdb81753
SAHIL SOAHIL <sahilsohailofcl@gmail.com>
2025-03-29 12:18:24 +0000

    Update page.tsx

---
+++ src/app/portfolio/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import { Header } from "../sections/Header";
+import { Footer } from "../sections/Footer";
+
+export default function YourComponent() {
+  return (
+    <>
+      <Header />
+        <div>
+          Portfolio  
+        <div/>
+      <Footer />
+    </>
+  );
+}
