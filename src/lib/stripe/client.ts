35aec8bdbbe04eb0fe42430ad415aa5512fb735f
sahilsohailofcl <sahilsohailofcl@gmail.com>
2025-04-15 20:31:09 +0100

    CE#11

--- src/lib/stripe/client.ts
+++ src/lib/stripe/client.ts
@@ -1,6 +1,10 @@
 import Stripe from 'stripe';
 
-export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-03-31.basil',
+if (!process.env.STRIPE_SECRET_KEY) {
+  throw new Error('Missing STRIPE_SECRET_KEY environment variable');
+}
+
+export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
+  apiVersion: '2023-10-16',
   typescript: true,
 });
